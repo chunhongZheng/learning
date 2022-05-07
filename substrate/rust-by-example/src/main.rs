66535925a0f76aaa5d552c2ef769bc51ec2c1c74
chunhongZheng <824394795@qq.com>
2022-05-07 16:14:03 +0800

    增加hello-world, trait-folder

--- substrate/rust-by-example/src/main.rs
+++ substrate/rust-by-example/src/main.rs
@@ -1,3 +1,23 @@
+mod trait_folder;
+mod hello_world;
+use crate::hello_world::format_print::format_print_fn;
+//此处引进traitFoler目录下的module
+// use crate::trait_folder::return_trait::test_return_trait;
+
 fn main() {
-    println!("Hello, world!");
+
+    /// traitExample main testing  start
+    // Type annotation is necessary in this case.
+    // let mut dolly: Sheep = Animal::new("Dolly");
+    // //
+    // dolly.talk();
+    // dolly.shear();
+    // dolly.talk();
+    // // traitExample main testing  end
+
+    // testDerive();
+    //  test_return_trait();
+    format_print_fn();
+    //  test_return_trait();
 }
+
