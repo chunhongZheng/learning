569c426ff6c74abab5eab83ab6df06f4266e18e1
chunhongZheng <824394795@qq.com>
2022-05-07 16:10:37 +0800

    111

---
+++ substrate/rust-by-example/target/debug/deps/rust_by_example.d
@@ -0,0 +1,5 @@
+D:\CLionProjects\learning\substrate\rust-by-example\target\debug\deps\rust_by_example.exe: src\main.rs
+
+D:\CLionProjects\learning\substrate\rust-by-example\target\debug\deps\rust_by_example.d: src\main.rs
+
+src\main.rs:
---
+++ substrate/rust-by-example/target/debug/rust-by-example.d
@@ -0,0 +1,1 @@
+D:\CLionProjects\learning\substrate\rust-by-example\target\debug\rust-by-example.exe: D:\CLionProjects\learning\substrate\rust-by-example\src\main.rs
